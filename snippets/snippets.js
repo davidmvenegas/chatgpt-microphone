31678b44b693c5be81fa97a9c31a0af65a28fe84
David Venegas <venegasdavidm@gmail.com>
2023-04-30 04:32:39 -0400

    more snippets work

--- snippets/snippets.js
+++ snippets/snippets.js
@@ -2,26 +2,15 @@ document.getElementById('closeSnippets').addEventListener('click', () => {
     window.close();
 });
 
+// state variables
+let isEditing = false;
+
+// select elements
 const tableContainer = document.getElementById('snippetsTable');
 const addNewButton = document.getElementById('addNewSnippet');
 const editButton = document.getElementById('editSnippets');
 const saveButton = document.getElementById('saveSnippets');
-
-// clean pasted text
-document.querySelectorAll('[data-paste]').forEach(element => {
-    element.addEventListener('paste', (e) => {
-        e.preventDefault();
-        const plainText = (e.clipboardData || window.Clipboard).getData('text/plain');
-        const selection = window.getSelection();
-        const range = selection.getRangeAt(0);
-        range.deleteContents();
-        const textNode = document.createTextNode(plainText);
-        range.insertNode(textNode);
-        range.setStartAfter(textNode);
-        selection.removeAllRanges();
-        selection.addRange(range);
-    });
-});
+const deleteIcons = document.getElementsByClassName('delete-icon');
 
 // create new row
 function createNewRow() {
@@ -30,22 +19,55 @@ function createNewRow() {
     newRow.innerHTML = `
         <div class="cell"><div class="SS_shortcut auto-grow" contenteditable="true" data-paste></div></div>
         <div class="cell"><div class="SS_snippet auto-grow" contenteditable="true" data-paste></div></div>
+        <img src="../assets/delete.svg" class="delete-icon" />
     `;
     tableContainer.appendChild(newRow);
+    newRow.querySelector('.SS_shortcut').addEventListener('paste', handlePaste);
+    newRow.querySelector('.SS_snippet').addEventListener('paste', handlePaste);
 }
 
-// load data
-function loadData() {
-    chrome.storage.sync.get('data', ({ data }) => {
-        console.log('Data loaded', data);
-        data.forEach(item => {
-            createNewRow();
-            const shortcuts = document.getElementsByClassName('SS_shortcut');
-            const snippets = document.getElementsByClassName('SS_snippet');
-            shortcuts[shortcuts.length - 1].innerText = item.shortcut;
-            snippets[snippets.length - 1].innerText = item.snippet;
-        });
-    });
+// clean pasted text
+function handlePaste(e) {
+    e.preventDefault();
+    console.log('pasted')
+    const plainText = e.clipboardData.getData('text/plain');
+    const selection = window.getSelection();
+    const range = selection.getRangeAt(0);
+    range.deleteContents();
+    const textNode = document.createTextNode(plainText);
+    range.insertNode(textNode);
+    range.setStartAfter(textNode);
+    selection.removeAllRanges();
+    selection.addRange(range);
+}
+
+// toggle edit icons
+function toggleEditIcons() {
+    if (isEditing) {
+        hideEditIcons();
+        isEditing = false;
+    } else {
+        showEditIcons();
+        isEditing = true;
+    }
+}
+
+// show edit icons
+function showEditIcons() {
+    editButton.innerText = 'Done';
+    for (let i = 0; i < deleteIcons.length; i++) {
+        deleteIcons[i].style.display = 'inherit';
+    }
+    isEditing = true;
+}
+
+// hide edit icons
+function hideEditIcons() {
+    editButton.innerText = 'Edit';
+    for (let i = 0; i < deleteIcons.length; i++) {
+        deleteIcons[i].style.display = 'none';
+    }
+    isEditing = false;
 }
 
 // save data
@@ -64,8 +86,23 @@ function saveData() {
     });
 }
 
+// load data
+function loadData() {
+    chrome.storage.sync.get('data', ({ data }) => {
+        data.forEach(item => {
+            createNewRow();
+            const shortcuts = document.getElementsByClassName('SS_shortcut');
+            const snippets = document.getElementsByClassName('SS_snippet');
+            shortcuts[shortcuts.length - 1].innerText = item.shortcut;
+            snippets[snippets.length - 1].innerText = item.snippet;
+        });
+    });
+}
+
 // event listeners
 addNewButton.addEventListener('click', createNewRow);
 saveButton.addEventListener('click', saveData);
+editButton.addEventListener('click', toggleEditIcons);
+
 
 loadData();
