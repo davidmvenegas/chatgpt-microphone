dc0325170f3a0d1a02e6ac7ec6468cb3db225b5f
David Venegas <venegasdavidm@gmail.com>
2023-04-30 22:32:54 -0400

    snippets create and delete

--- snippets/snippets.js
+++ snippets/snippets.js
@@ -4,32 +4,45 @@ document.getElementById('closeSnippets').addEventListener('click', () => {
 
 // state variables
 let isEditing = false;
+let deletedRowsQueue = [];
 
 // select elements
 const tableContainer = document.getElementById('snippetsTable');
+const cancelNewButton = document.getElementById('cancelNewSnippet');
+const saveNewButton = document.getElementById('saveNewSnippet');
 const addNewButton = document.getElementById('addNewSnippet');
 const editButton = document.getElementById('editSnippets');
 const saveButton = document.getElementById('saveSnippets');
 const deleteIcons = document.getElementsByClassName('delete-icon');
 
 // create new row
-function createNewRow() {
+function createNewRow(fromExistingData) {
     const newRow = document.createElement('div');
-    newRow.classList.add('row');
+    newRow.classList.add('snippet-row');
     newRow.innerHTML = `
         <div class="cell"><div class="SS_shortcut auto-grow" contenteditable="true" data-paste></div></div>
         <div class="cell"><div class="SS_snippet auto-grow" contenteditable="true" data-paste></div></div>
         <img src="../assets/delete.svg" class="delete-icon" />
     `;
     tableContainer.appendChild(newRow);
+    // add paste and delete event listeners
     newRow.querySelector('.SS_shortcut').addEventListener('paste', handlePaste);
     newRow.querySelector('.SS_snippet').addEventListener('paste', handlePaste);
+    newRow.querySelector('.delete-icon').addEventListener('click', handleDeleteClick);
+    console.log('fromExistingData', fromExistingData)
+    if (!fromExistingData) {
+        console.log('new row that is not from existing data')
+        editButton.style.display = 'none';
+        saveButton.style.display = 'none';
+        cancelNewButton.style.display = 'inherit';
+        saveNewButton.style.display = 'inherit';
+        newRow.querySelector('.SS_shortcut').focus();
+    }
 }
 
 // clean pasted text
 function handlePaste(e) {
     e.preventDefault();
-    console.log('pasted')
     const plainText = e.clipboardData.getData('text/plain');
     const selection = window.getSelection();
     const range = selection.getRangeAt(0);
@@ -41,68 +54,116 @@ function handlePaste(e) {
     selection.addRange(range);
 }
 
-// toggle edit icons
-function toggleEditIcons() {
+// toggle editing
+function toggleEditing() {
     if (isEditing) {
-        hideEditIcons();
-        isEditing = false;
+        endEditing(false);
     } else {
-        showEditIcons();
-        isEditing = true;
+        startEditing();
     }
 }
 
-// show edit icons
-function showEditIcons() {
-    editButton.innerText = 'Done';
+// start editing
+function startEditing() {
+    editButton.innerText = 'Cancel';
+    addNewButton.style.display = 'none';
     for (let i = 0; i < deleteIcons.length; i++) {
         deleteIcons[i].style.display = 'inherit';
     }
     isEditing = true;
 }
 
-// hide edit icons
-function hideEditIcons() {
+// handle delete click
+function handleDeleteClick(event) {
+    const rowToDelete = event.target.parentNode;
+    const rowIndex = Array.from(tableContainer.children).indexOf(rowToDelete);
+    const shortcutValue = rowToDelete.querySelector('.SS_shortcut').innerText;
+    const isDeleted = deletedRowsQueue.some(row => row.shortcut === shortcutValue);
+    const shortcutCell = rowToDelete.children[0];
+    const snippetCell = rowToDelete.children[1];
+    if (isDeleted) {
+        shortcutCell.classList.remove('deleted-cell');
+        snippetCell.classList.remove('deleted-cell');
+        deletedRowsQueue = deletedRowsQueue.filter(row => row.shortcut !== shortcutValue);
+    } else {
+        shortcutCell.classList.add('deleted-cell');
+        snippetCell.classList.add('deleted-cell');
+        deletedRowsQueue.push({ element: rowToDelete, rowIndex: rowIndex, shortcut: shortcutValue });
+    }
+    console.log(deletedRowsQueue);
+}
+
+// end editing
+function endEditing(saveChanges) {
     editButton.innerText = 'Edit';
+    addNewButton.style.display = 'inherit';
     for (let i = 0; i < deleteIcons.length; i++) {
         deleteIcons[i].style.display = 'none';
+    }
+    // if saveChanges, delete rows marked for deletion
+    if (saveChanges) {
+        deletedRowsQueue.forEach(row => row.element.remove());
+    } else {
+        // else, return rows to normal
+        deletedRowsQueue.forEach(row => {
+            row.element.children[0].classList.remove('deleted-cell');
+            row.element.children[1].classList.remove('deleted-cell');
+        });
+        deletedRowsQueue = [];
     }
     isEditing = false;
 }
 
 // save data
 function saveData() {
+    if (isEditing) endEditing(true);
     const shortcuts = document.getElementsByClassName('SS_shortcut');
     const snippets = document.getElementsByClassName('SS_snippet');
-    const data = [];
+    const snippetsData = [];
     for (let i = 0; i < shortcuts.length; i++) {
-        data.push({
-            shortcut: shortcuts[i].innerText,
-            snippet: snippets[i].innerText
-        });
+        const shortcutValue = shortcuts[i].innerText;
+        // only include rows that are not marked for deletion
+        if (!deletedRowsQueue.some(row => row.shortcut === shortcutValue)) {
+            snippetsData.push({
+                shortcut: shortcutValue,
+                snippet: snippets[i].innerText
+            });
+        }
     }
-    chrome.storage.sync.set({ data }, () => {
+    deletedRowsQueue = [];
+    chrome.storage.sync.set({ snippetsData: snippetsData }, () => {
         console.log('Data saved');
+        // if no data, create a starting row
+        console.log('Data: ', snippetsData)
+        if (snippetsData.length === 0) {
+            createNewRow(false);
+        }
     });
 }
 
 // load data
 function loadData() {
-    chrome.storage.sync.get('data', ({ data }) => {
-        data.forEach(item => {
-            createNewRow();
-            const shortcuts = document.getElementsByClassName('SS_shortcut');
-            const snippets = document.getElementsByClassName('SS_snippet');
-            shortcuts[shortcuts.length - 1].innerText = item.shortcut;
-            snippets[snippets.length - 1].innerText = item.snippet;
-        });
+    chrome.storage.sync.get('snippetsData', ({ snippetsData }) => {
+        // if no data, create a starting row
+        if (!snippetsData || snippetsData.length === 0) {
+            createNewRow(false);
+        } else {
+            // else, create rows for each item in snippetsData
+            snippetsData.forEach(item => {
+                createNewRow(true);
+                const shortcuts = document.getElementsByClassName('SS_shortcut');
+                const snippets = document.getElementsByClassName('SS_snippet');
+                shortcuts[shortcuts.length - 1].innerText = item.shortcut;
+                snippets[snippets.length - 1].innerText = item.snippet;
+            });
+        }
     });
 }
 
 // event listeners
-addNewButton.addEventListener('click', createNewRow);
-saveButton.addEventListener('click', saveData);
-editButton.addEventListener('click', toggleEditIcons);
+saveButton.addEventListener('click', () => saveData());
+editButton.addEventListener('click', () => toggleEditing());
+addNewButton.addEventListener('click', () => createNewRow(false));
 
 
 loadData();
