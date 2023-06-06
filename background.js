1a1f4072ce14071111d5163ef57be9e989f9faa7
David Venegas <venegasdavidm@gmail.com>
2023-06-06 00:27:27 -0400

    changing install

--- background.js
+++ background.js
@@ -1,4 +1,8 @@
-// open snippets page on install
-self.addEventListener('install', () => {
-    chrome.tabs.create({ url: 'snippets/snippets.html' });
+chrome.runtime.onInstalled.addListener((details) => {
+    // open snippets page on install
+    if (details.reason === 'install') {
+        chrome.tabs.create({ url: 'snippets/snippets.html' });
+    } else if (details.reason === 'update') {
+        // nothing here for now
+    }
 });
